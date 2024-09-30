98bc9949324cbeab72079e5c597431f4ef73fb4e
RAJU <RAJU@RAJU>
2024-09-30 12:35:47 +0530

    Price book Link is added to In home page(POM).

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/homepage/HomePage.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/homepage/HomePage.java
@@ -8,73 +8,72 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
-	@FindBy(linkText="Organizations")
+	@FindBy(linkText = "Organizations")
 	private WebElement orgLink;
-	
+
 	@FindBy(xpath = "//a[@href='index.php?module=Assets&action=index']")
 	private WebElement assetsLink;
-	
+
 	@FindBy(xpath = "//a[@name='Purchase Order']")
 	private WebElement purchaseOrderLink;
-	
-	public WebElement getPurchaseOrderLink() {
-		return purchaseOrderLink;
-	}
-	@FindBy(linkText="Contacts")
+
+	@FindBy(linkText = "Contacts")
 	private WebElement contactLink;
-	
-	@FindBy(xpath ="//a[@name='Sales Order']")
+
+	@FindBy(xpath = "//a[@name='Sales Order']")
 	private WebElement salesOrderLink;
-	
-	public WebElement getSalesOrderLink() {
-		return salesOrderLink;
-	}
-	@FindBy(name="Invoice")
+
+	@FindBy(xpath = "//a[@href='index.php?module=PriceBooks&action=index']")
+	private WebElement priceBooksLink;
+
+	@FindBy(name = "Invoice")
 	private WebElement invoiceLink;
 
 	public WebElement getAssetsLink() {
 		return assetsLink;
 	}
 
+	@FindBy(xpath = "//a[text()='Opportunities']") // a[@name='Sales Order']
+	private WebElement opportunitiesLink;
+
+	@FindBy(linkText = "Products")
+	private WebElement productLink;
+
+	@FindBy(linkText = "More")
+	private WebElement moreLink;
+
+	@FindBy(linkText = "Campaigns")
+	private WebElement campaignsLink;
+
+	@FindBy(xpath = "//img[@src='themes/softed/images/user.PNG']")
+	private WebElement adminImg;
+
+	@FindBy(linkText = "Sign Out")
+	private WebElement signOutLink;
+
+	// object initialization
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement getPriceBooksLink() {
+		return priceBooksLink;
+	}
 	public WebElement getInvoiceLink() {
 		return invoiceLink;
 	}
 
-	@FindBy(xpath = "//a[text()='Opportunities']")//a[@name='Sales Order']
-	private WebElement opportunitiesLink;
-	
-	@FindBy(linkText="Products")
-	private WebElement productLink;
-	
-	public WebElement getOpportunitiesLink() {
-		return opportunitiesLink;
+	public WebElement getPurchaseOrderLink() {
+		return purchaseOrderLink;
 	}
 
-	public WebElement getProductLink() {
-		return productLink;
-	}
-
-	@FindBy(linkText="More")
-	private WebElement moreLink;
-	
-	public WebElement getMoreLink() {
-		return moreLink;
-	}
-
-	@FindBy(linkText="Campaigns")
-	private WebElement campaignsLink;
-	
-	
-	@FindBy(xpath = "//img[@src='themes/softed/images/user.PNG']")
-	private WebElement adminImg;
-	
-	@FindBy(linkText="Sign Out")
-	private WebElement signOutLink;
-	
-	// object initialization
-	
 	public WebElement getAdminImg() {
 		return adminImg;
+	}
+
+	public WebElement getSalesOrderLink() {
+		return salesOrderLink;
 	}
 
 	public WebElement getSignOutLink() {
@@ -85,13 +84,19 @@ public class HomePage {
 		return campaignsLink;
 	}
 
-	WebDriver driver;
-	
-	public HomePage(WebDriver driver)
-	{
-		this.driver=driver;
-		PageFactory.initElements(driver, this);
+	public WebElement getOpportunitiesLink() {
+		return opportunitiesLink;
 	}
+
+	public WebElement getProductLink() {
+		return productLink;
+	}
+
+	public WebElement getMoreLink() {
+		return moreLink;
+	}
+
+	WebDriver driver;
 
 	public WebElement getOrgLink() {
 		return orgLink;
@@ -100,35 +105,29 @@ public class HomePage {
 	public WebElement getContactLink() {
 		return contactLink;
 	}
-	
-	public void navigateToCampaingn()
-	{
-		Actions act=new Actions(driver);
+
+	public void navigateToCampaingn() {
+		Actions act = new Actions(driver);
 		act.moveToElement(moreLink).perform();
 		campaignsLink.click();
 	}
+
 	@FindBy(linkText = "Vendors")
 	private WebElement venderLink;
 
 	public WebElement getVenderLink() {
 		return venderLink;
 	}
-	public void navigateToVender()
-	{
-		Actions act =new Actions(driver);
+
+	public void navigateToVender() {
+		Actions act = new Actions(driver);
 		act.moveToElement(moreLink).perform();
 		venderLink.click();
 	}
 
-	
-	public void signOut()
-	{
+	public void signOut() {
 		adminImg.click();
 		signOutLink.click();
 	}
 
-	
-	
-	
-	
 }
